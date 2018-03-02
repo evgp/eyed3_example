3f763e0d5675119974b820bfffd46aee60768325
Evg P <evg.p@hotmail.com>
2018-03-02 14:12:26 +1200

    Added re.sub for u0161 symbol

--- mp3_+_lyrics.py
+++ mp3_+_lyrics.py
@@ -6,7 +6,7 @@ import re, eyed3, os, io
 lectures = {} # num: file
 podcasts = {} # num: file
 
-file_path = '/mnt/c/dev/scrapy_eslpod/scrapy_selenium_test/Cultural English 1 - 100/'
+file_path = '/mnt/c/dev/scrapy_eslpod/scrapy_selenium_test/Cultural English 301 - 400/'
 file_list = os.listdir(file_path)
 
 for file_name in file_list:
@@ -22,7 +22,7 @@ for file_name in file_list:
         })
 
 for pod_key in podcasts:
-    try:
+    # try:
         pprint("Processing file " + podcasts[pod_key].decode('utf-8'))
         pod = eyed3.load(file_path + podcasts[pod_key])
         with io.open(file_path+lectures[pod_key], mode='r', encoding='utf-8') as f:
@@ -31,10 +31,11 @@ for pod_key in podcasts:
         lyric = re.sub(u"(\u2018|\u2019)", "'", lyric)
         lyric = re.sub(u"(\u2013|\u2014)", "-", lyric)        
         lyric = re.sub(u"(\u2026)", "...", lyric)        
+        lyric = re.sub(u"(\u0161)", "s", lyric)
         new_name = unicode(lectures[pod_key])
         pod.tag.title = new_name[:-4]
         pod.tag.lyrics[0].text = unicode(lyric)
         pod.tag.save()
         pod.rename(new_name[:-4])
-    except:
-        pprint("!!! Errors with file: " + lectures[pod_key].decode('utf-8'))
+    # except:
+    #     pprint("!!! Errors with file: " + lectures[pod_key].decode('utf-8'))
